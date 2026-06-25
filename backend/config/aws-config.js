ac8bc9747135bf3a3da2b800aa308d27c6d513c5
Mohammed Parkar <parkarmohammed74@gmail.com>
2026-06-26 02:15:26 +0530

    added the push command logic

--- backend/config/aws-config.js
+++ backend/config/aws-config.js
@@ -1,9 +1,15 @@
+require("dotenv").config();
 const AWS = require("aws-sdk");
 
-AWS.config.update({region : "ap-south-1"});
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
+});
 
 const s3 = new AWS.S3();
 
-const S3_BUCKET = "mera-buckett";
-
-module.exports = {s3, S3_BUCKET};
+module.exports = {
+  s3,
+  S3_BUCKET: process.env.S3_BUCKET,
+};
--- backend/controllers/push.js
+++ backend/controllers/push.js
@@ -1,5 +1,41 @@
-async function pushRepo(){
-    console.log("push command was called")
+const fs = require("fs").promises;
+const path = require("path");
+const { s3, S3_BUCKET } = require("../config/aws-config");
+
+async function pushRepo() {
+  const initPath = path.resolve(process.cwd(), ".VersionFlow");
+  const commitsPath = path.join(initPath, "commits");
+
+  try {
+    // reading the (multiple)commit folder inside the commits folder 
+    const commitDirs = await fs.readdir(commitsPath);
+    for(const commitDir of commitDirs){
+        const commitPath = path.join(commitsPath, commitDir);
+        const files = await fs.readdir(commitPath);
+
+        // now reading the each file of the each commit folder within commits folder (nested loopoing) 
+        for (const file of files) {
+            const filePath = path.join(commitPath,file);
+            const fileContent = await fs.readFile(filePath);
+            
+
+            // now uploading the file into s3 bucket 
+            const params = {
+                Bucket : S3_BUCKET,
+                Key : `commits/${commitDir}/${file}`,
+                Body : fileContent,
+            };
+
+            await s3.upload(params).promise(); 
+
+            console.log("The file were pushed to the S3 bucket");
+            
+        }
+    }
+
+  } catch (error) {
+    throw error;
+};
 }
 
-module.exports = {pushRepo};
+module.exports = { pushRepo };
