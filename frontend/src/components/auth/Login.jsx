705f050dad5e9ded9865a8dcd6abad3e2e816e25
Mohammed Parkar <parkarmohammed74@gmail.com>
2026-07-02 03:03:56 +0530

    same login ui code added

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -1,7 +1,116 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useAuth } from "../../authContext";
 
-const Login = ()=> {
-    return <h1> Login</h1>
+import "./auth.css";
+
+import { Button, Heading } from "@primer/react";   
+import logo from "../../../src/assets/github-mark-white.svg";
+import { Link } from "react-router-dom";
+
+const Login = () => {
+
+const { setCurrentUser } = useAuth();
+
+    useEffect(()=>{
+        localStorage.removeItem("token");
+        localStorage.removeItem("userId");
+        setCurrentUser(null);
+    },[])
+
+    const [email,setEmail] = useState("");
+    const [password,setPassword] = useState("");
+    const [loading,setLoading] = useState(false);
+
+    const handlelogin = async () => {
+        setLoading(true);
+        
+        try{
+            let res = await axios.post("http://localhost:3000/login",{
+                email : email,
+                password : password
+            });
+
+            // now as we login we get token , in response 
+            // we store it in localStorage and set it as current user
+            localStorage.setItem("token",res.data.token);
+            localStorage.setItem("userId",res.data.userId);
+
+            // and now set our current user as response userId
+            setCurrentUser(res.data.userId);
+
+            // proccess over
+            setLoading(false);
+
+            // finally we redirect our login user to home 
+            window.location.href = "/";
+
+
+        }catch(error){
+            console.error(error);
+            alert("Login Failed!");
+            setLoading(false);
+        }
+    }
+    
+
+  return (
+    <div className="login-wrapper">
+      <div className="login-logo-container">
+        <img className="logo-login" src={logo} alt="Logo" />
+      </div>
+
+      <div className="login-box-wrapper">
+       <div className="login-heading">
+  <h2>Login</h2>
+</div>
+
+        <div className="login-box">
+
+          <div>
+            <label className="label">Email address</label>
+            <input
+              autoComplete="off"
+              name="Email"
+              id="Email"
+              className="input"
+              type="email"
+              value = {email}
+              onChange={(e)=>{setEmail(e.target.value)}}
+            />
+          </div>
+
+          <div className="div">
+            <label className="label">Password</label>
+            <input
+              autoComplete="off"
+              name="Password"
+              id="Password"
+              className="input"
+              type="password"
+              value={password}
+              onChange = {(e)=>{setPassword(e.target.value)}}
+            />
+          </div>
+
+          <Button
+            variant="primary"
+            className="login-btn"
+            onClick={handlelogin}
+            disabled = {loading}
+          >
+            {loading ? "Loading...." : "Login"}
+          </Button>
+        </div>
+
+        <div className="pass-box">
+          <p>
+            New to VersionFlow ? <Link to="/signup">Create an account</Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- frontend/src/components/auth/Signup.jsx
+++ frontend/src/components/auth/Signup.jsx
@@ -106,7 +106,7 @@ const Signup = () => {
 
         <div className="pass-box">
           <p>
-            Already have an account? <Link to="/auth">Login</Link>
+            Already have an account? <Link to="/login">Login</Link>
           </p>
         </div>
       </div>
